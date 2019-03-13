0852a3002a316a7a15005745d6c0704980638171
Artem Karmaz <inste@icloud.com>
2019-03-13 14:54:43 +0200

    Core data +

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -40,6 +40,7 @@ class CurrentLocationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabels()
+        print("URL - \(getUrl())")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -285,6 +286,19 @@ extension CurrentLocationViewController {
         }
         
         return line1 + "\n" + line2
+    }
+    
+    // Find Core Data Location in Library
+    
+    private func getUrl(path: String = "") -> URL {
+        var url = URL(fileURLWithPath: path)
+        do {
+            url = try FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(path)
+        } catch {
+            print(error)
+        }
+        
+        return url
     }
 }
 
