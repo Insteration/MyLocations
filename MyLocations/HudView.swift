6c80247b4287cbcb2372a43a9607dba7e7728744
Artem Karmaz <inste@icloud.com>
2019-03-13 12:51:58 +0200

    added animations

--- MyLocations/HudView.swift
+++ MyLocations/HudView.swift
@@ -22,8 +22,9 @@ class HudView: UIView {
         view.addSubview(hudView)
         view.isUserInteractionEnabled = false
         
-//        hudView.backgroundColor = UIColor(displayP3Red: 1, green: 0, blue: 0, alpha: 0.5)
+        //        hudView.backgroundColor = UIColor(displayP3Red: 1, green: 0, blue: 0, alpha: 0.5)
         
+        hudView.show(animated: animated)
         return hudView
     }
     
@@ -44,5 +45,39 @@ class HudView: UIView {
         UIColor(white: 0.3, alpha: 0.8).setFill()
         roundRect.fill()
         
+        // Draw checkmark
+        if let image = UIImage(named: "Checkmark") {
+            let imaePoint = CGPoint (
+                x: center.x - round(image.size.width / 2),
+                y: center.y - round(image.size.height / 2) - boxHeight / 8)
+            image.draw(at: imaePoint)
+        }
+        
+        // Draw the text
+        let attribs = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+        
+        let textSize = text.size(withAttributes: attribs)
+        
+        let textPoint = CGPoint(
+            x: center.x - round(textSize.width / 2),
+            y: center.y - round(textSize.height / 2) + boxHeight / 4)
+        
+        text.draw(at: textPoint, withAttributes: attribs)
+    }
+    
+    // MARK: Public Methods
+    
+    func show(animated: Bool) {
+        if animated {
+            alpha = 0
+            transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: [], animations: {
+                self.alpha = 1
+                self.transform = CGAffineTransform.identity
+            }, completion: nil)
+        }
     }
 }
